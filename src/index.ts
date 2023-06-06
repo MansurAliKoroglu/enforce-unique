592d24fd7481a49faff274a3bc535b43ab90df1b
Mansur Ali Koroglu <mansuralikoroglu@gmail.com>
2023-06-07 01:34:12 +0300

    Rename unique error file to enforce unique error

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { UniqueEnforcer } from './unique-enforcer';
-export { EnforceUniqueError } from './errors/unique.error';
+export { EnforceUniqueError } from './errors/enforce-unique.error';
 export { UniqueComparable } from './definitions/unique-comparable';
